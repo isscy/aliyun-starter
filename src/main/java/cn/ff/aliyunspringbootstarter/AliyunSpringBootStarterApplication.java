28d620b980761c00189c83a6cc602d82f60f5bb0
fengfan <dotff@qq.com>
2018-11-07 08:58:05 +0800

    init

---
+++ src/main/java/cn/ff/aliyunspringbootstarter/AliyunSpringBootStarterApplication.java
@@ -0,0 +1,12 @@
+package cn.ff.aliyunspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliyunSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliyunSpringBootStarterApplication.class, args);
+    }
+}
